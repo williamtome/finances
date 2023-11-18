aad1b535a3588f1e7314107ae0105b24180a86e5
William Tomé <william.weirich@helioprint.com.br>
2023-11-18 19:05:13 -0300

    (test) Delete revenue test created

---
+++ tests/Feature/DeleteRevenueTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Revenue;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class DeleteRevenueTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_delete_revenue_successful(): void
+    {
+        $revenue = Revenue::factory()->create();
+
+        $this->delete('/api/revenues/' . $revenue->id)
+            ->assertStatus(Response::HTTP_OK);
+
+        $this->assertDatabaseMissing(Revenue::class, [$revenue->id]);
+    }
+
+    public function test_should_be_generate_error_on_delete_revenue_unknown(): void
+    {
+        $revenue = Revenue::find(777);
+
+        $this->expectException(\ErrorException::class);
+        $this->expectExceptionMessage('Attempt to read property "id" on null');
+
+        $this->delete('/api/revenues/' . $revenue->id)
+            ->assertStatus(Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+}
