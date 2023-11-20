25e3d9e2a8a49c39c1210081b56de92dbee27b6f
William Tomé <william.weirich@helioprint.com.br>
2023-11-19 22:04:19 -0300

    (test) create expense tests

--- tests/Feature/CreateExpenseTest.php
+++ tests/Feature/CreateExpenseTest.php
@@ -12,20 +12,20 @@ class CreateExpenseTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_create_expense_successful()
+    public function test_create_expense_successful(): void
     {
         $data = [
             'description' => 'Despesa de teste',
             'amount' => 50,
         ];
 
-        $this->post(route('expenses.store'), $data)
+        $this->post(route('expenses.store'), $data, ['Accept' => 'application/json'])
             ->assertStatus(Response::HTTP_OK);
 
         $this->assertDatabaseHas(Expense::class, $data);
     }
 
-    public function test_should_be_generate_error_on_create_expense_no_data()
+    public function test_should_be_generate_error_on_create_expense_no_data(): void
     {
         $this->post(route('expenses.store'), [], ['Accept' => 'application/json'])
             ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
@@ -35,7 +35,7 @@ class CreateExpenseTest extends TestCase
             ]);
     }
 
-    public function test_should_br_generate_error_when_the_description_field_is_incorrect()
+    public function test_should_br_generate_error_when_the_description_is_incorrect(): void
     {
         $data = [
             'description' => 1234,
@@ -47,6 +47,21 @@ class CreateExpenseTest extends TestCase
             ->assertJsonValidationErrors([
                 'description' => 'The description field must be a string.',
                 'description' => 'The description field must be at least 5 characters.',
+            ]);
+    }
+
+    public function test_should_be_generate_error_when_the_amount_is_incorrect(): void
+    {
+        $data = [
+            'description' => 'Despesa teste',
+            'amount' => 'abc',
+        ];
+
+        $this->post(route('expenses.store'), $data, ['Accept' => 'application/json'])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonValidationErrors([
+                'amount' => 'The amount field must be a number.',
+                'amount' => 'The amount field must be greater than or equal to 1.',
             ]);
     }
 }
