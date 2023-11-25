6ad47c97af2fdbe30d3fcb427586a754a9bfae50
William Tomé <william.weirich@helioprint.com.br>
2023-11-24 23:59:03 -0300

    (feat) amount value formatted

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -14,4 +15,11 @@ class Expense extends Model
         'description',
         'amount',
     ];
+
+    protected function amount(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => (float) $value,
+        );
+    }
 }
