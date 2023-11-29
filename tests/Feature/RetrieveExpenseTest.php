3e08646e2fe6ed2e3470ad7b865a6385fe1587ec
William Tomé <william.weirich@helioprint.com.br>
2023-11-28 23:09:56 -0300

    (test) retrieve expense created

---
+++ tests/Feature/RetrieveExpenseTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Expense;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class RetrieveExpenseTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_list_all_expenses_successful()
+    {
+        Expense::factory()->count(5)->create();
+
+        $this->get(route('expenses.index'), ['Content-Type' => 'application/json'])
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                [
+                    'id',
+                    'description',
+                    'amount',
+                    'created_at',
+                    'updated_at',
+                    'deleted_at',
+                ]
+            ]);
+    }
+}
