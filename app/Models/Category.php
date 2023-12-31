d43ab0c99f11cd63da1f74e62247ed4a971c79fb
William Tomé <william.weirich@helioprint.com.br>
2023-12-31 19:05:26 -0300

    (feat) added category id in model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,15 @@ class Category extends Model
 {
     use HasFactory, SoftDeletes;
 
+    public const FOOD = 1; // ALIMENTAÇÃO
+    public const HEALTH = 2; // SAÚDE
+    public const HOME = 3; // MORADIA
+    public const TRANSPORT = 4; // TRANSPORTE
+    public const EDUCATION = 5; // EDUCAÇÃO
+    public const LEISURE = 6; // LAZER
+    public const UNEXPECTED = 7; // IMPREVISTOS
+    public const OTHERS = 8; // OUTRAS
+
     protected $fillable = [
         'name'
     ];
