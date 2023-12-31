55a57967cc635e41bb0a986ef97440d44cfb92fa
William Tomé <william.weirich@helioprint.com.br>
2023-12-31 19:20:19 -0300

    (test) run seeders in database tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,13 @@
 
 namespace Tests;
 
+use Database\Seeders\TestDatabaseSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $seed = true;
+    protected $seeder = TestDatabaseSeeder::class;
 }
